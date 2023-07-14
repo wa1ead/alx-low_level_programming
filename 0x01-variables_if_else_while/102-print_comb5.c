537cd9c4f364a2611f2ae589082e03301f2e7504
wa1ead <waliddraa2@gmail.com>
2023-07-14 11:56:03 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 			putchar((n % 10) + '0');
 			putchar(' ');
 			putchar((m / 10) + '0');
-			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
 			if (n == 98 && m == 99)
 				continue;
 		}
