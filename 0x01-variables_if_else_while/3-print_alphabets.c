95a64737849952865c19a0760c1efa0fa17fa3b7
wa1ead <waliddraa2@gmail.com>
2023-07-14 08:48:32 -0700

    7. Intel

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,26 @@
-g
+#include <stdio.h>
+/**
+* main - entry point
+* while - loop
+* Return: always 0
+*/
+int main(void)
+{
+	int n = 97;
+	int m = 65
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
