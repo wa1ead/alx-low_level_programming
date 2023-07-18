048cd36490cfc2afc37a399ea21d5d395386f709
wa1ead <waliddraa2@gmail.com>
2023-07-18 03:17:30 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * main - entry point
 * putchar - print function
@@ -6,5 +6,14 @@
 */
 int main(void)
 {
-	putchar("_putchar\n")
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0)
 }
