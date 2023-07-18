de8b79b63918d107d0e1996e8b5618d937b61fc2
wa1ead <waliddraa2@gmail.com>
2023-07-18 06:25:26 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
-			if (k < 0 && j != 0)
+			else if (k < 0 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -32,6 +32,6 @@ void times_table(void)
 				_putchar((k % 10) + '0');
 			}
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
