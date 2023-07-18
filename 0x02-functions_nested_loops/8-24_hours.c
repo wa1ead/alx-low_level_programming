b917de2f51dd07ff9166ae74d5ad49ae01a6d1f5
wa1ead <waliddraa2@gmail.com>
2023-07-18 04:34:30 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,14 +16,14 @@ void jack_bauer(void)
 			{
 				for (d = 0; d <= 9; d++)
 				{
-					if (a = 2 && b >= 4)
+					if (a >= 2 && b >= 4)
 						break;
-				_putchar(a + 48);
-				_putchar(b + 48);
-				_putchar(58);
-				_putchar(c + 48);
-				_putchar(d + 48);
-				_putchar('\n')
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(58);
+					_putchar(c + 48);
+					_putchar(d + 48);
+					_putchar('\n');
 				}
 			}
 		}
