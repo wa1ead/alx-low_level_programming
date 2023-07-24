7406f06dffd400012ea804d28bedb883e7756768
wa1ead <waliddraa2@gmail.com>
2023-07-24 07:28:14 -0700

    11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates random valid password
+ * Return: always 0
+ */
+int main(void)
+{
+	int pswd[100];
+	int a, b ,sum;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (a = 0; a < 100; a++)
+	{
+		pswd[a] = rand() % 78;
+		sum += (pass[a] + '0');
+		putchar(pass[a] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
