5b439786a7df5cb5ba712d5d058a804e45689a70
wa1ead <waliddraa2@gmail.com>
2023-07-26 08:59:45 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encodes string using rot13
+ * @s: string
+ * Return: always 0
+ */
+char *rot13(char *s)
+{
+	int x;
+	int y;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (s[x] == data1[y])
+			{
+				s[x] = datarot[y];
+				break;
+			}
+		}
+	}
+
+	return (s)
+}
