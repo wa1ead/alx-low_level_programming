59e1f32e1fb0bd4bcbddee9100492eedf1c41946
wa1ead <waliddraa2@gmail.com>
2023-08-02 06:57:11 -0700

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s)
+/**
+ * is_palindrome - checks for palindrome
+ * @s: string
+ * Return: always 0
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string
+ * Return: always 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks recursively for palindrome
+ * @s: string
+ * @i: iterator
+ * @len: length of string
+ * Return: always 0
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + 1) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,6 @@ int _sqrt_recursion(int n);
 int actual_sqrt_recursion(int n, int i);
 int is_prime_number(int n);
 int actual_prime(int n, int i);
+int is_palindrome(char *s);
 
 #endif
