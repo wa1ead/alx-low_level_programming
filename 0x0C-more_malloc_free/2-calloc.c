4c294cc9a03243c10451f3f2fe71836b59d4a1e5
wa1ead <waliddraa2@gmail.com>
2023-08-10 07:07:27 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,16 +22,17 @@ char *_memset(char *s, char b, unsigned int n)
  * @size: size
  * Return: always 0
  */
-void *_calloc -(unsigned int nmemb, unsigned int size)
+void *_calloc (unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	
+
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
+
 	_memset(ptr, 0, nmemb * size);
 
 	return (ptr);
