eb3d689900c17fa237f73d36d2a18f005cb8f0cb
wa1ead <waliddraa2@gmail.com>
2023-08-14 05:49:29 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,20 +14,6 @@ struct dog
 	char *owner;
 };
 
-/**
- * init_dog - initializes variable
- * @d: pointer
- * @name: var name
- * @age: var age
- * @owner: var owner
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
