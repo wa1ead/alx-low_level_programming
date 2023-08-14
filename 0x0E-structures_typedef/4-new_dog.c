c843bb731851993f0224ff03b561a7cefa190961
wa1ead <waliddraa2@gmail.com>
2023-08-14 06:12:36 -0700

    4. A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * _strlen - returns length of string
+ * @s: string to evaluate
+ * Return: always 0
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	return (x);
+}
+
+/**
+ * *_strcpy - copies the string
+ * @dest: pointer to the buffer
+ * @src: string copied
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, x;
+
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	for (x = 0; x < len; x++)
+		dest[x] = src[x];
+	dest[x] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - cretaes new dog
+ * @name: var name
+ * @age: var age
+ * @owner: var owner
+ * Return: always 0
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+		free(dog);
+		return (NULL);
+
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
